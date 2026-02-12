80e3076f795661946fa9b342ba55960f7c7ff32f
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2026-02-12 10:53:58 -0800

    Fixed wrong character for SQL

--- src/scripts/seed.sql
+++ src/scripts/seed.sql
@@ -1,2 +1,2 @@
-INSERT INTO 'products' ('ID', 'Name', 'Price', 'Color', 'Pattern', 'Brand', 'Size', 'Image') VALUES 
-    (1, "Basic T-Shirt", "20.00", "White", "Blank", "3-Shirts", "Medium", NULL);
+INSERT INTO 'products' (`ID`, `Name`, `Price`, `Color`, `Pattern`, `Brand`, `Size`, `Image`) VALUES 
+    (1, `Basic T-Shirt`, `20.00`, `White`, `Blank`, `3-Shirts`, `Medium`, NULL);
