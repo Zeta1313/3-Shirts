8ed6397edc93a928d12f32c7ab7e2ebec317756e
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2026-02-12 10:44:44 -0800

    Added Seed.SQL

---
+++ src/scripts/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO 'products' ('ID', 'Name', 'Price', 'Color', 'Pattern', 'Brand', 'Size', 'Image') VALUES {
+    (1, "Basic T-Shirt", "20.00", "White", "Blank", "3-Shirts", "Medium", NULL)
+}
