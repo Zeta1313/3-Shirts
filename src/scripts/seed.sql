92688576865509c27474c0c3e543ec3bd4c9fdea
allenres <allen.resulidze@gmail.com>
2026-02-20 11:07:39 -0800

    fixed seed syntax and spelling

--- src/scripts/seed.sql
+++ src/scripts/seed.sql
@@ -2,42 +2,42 @@
 INSERT INTO `products`
 (`Name`, `Description`, `Price`, `Color`, `Pattern`, `Brand`, `Size`, `Image`)
 VALUES
-(1, 'Classic Shirt', 'Classic white shirt made from high-quality cotton', 20.00, 'White', 'Solid', '3-Shirts', 'S', NULL),
+('Classic Shirt', 'Classic white shirt made from high-quality cotton', 20.00, 'White', 'Solid', '3-Shirts', 'S', NULL),
 
-(2, 'Classic Shirt', 'Classic white shirt made from high-quality cotton', 20.00, 'White', 'Solid', '3-Shirts', 'M', NULL),
+('Classic Shirt', 'Classic white shirt made from high-quality cotton', 20.00, 'White', 'Solid', '3-Shirts', 'M', NULL),
 
-(3, 'Classic Shirt', 'Classic white shirt made from high-quality cotton', 20.00, 'White', 'Solid', '3-Shirts', 'L', NULL),
+('Classic Shirt', 'Classic white shirt made from high-quality cotton', 20.00, 'White', 'Solid', '3-Shirts', 'L', NULL),
 
-(4, 'Classic Shirt - Colored', 'Classic shirt made from high-quality cotton. Availible in 26 different colors', 22.00, 'Varied', 'Solid', '3-Shirts', 'S', NULL),
+('Classic Shirt - Colored', 'Classic shirt made from high-quality cotton. Available in 26 different colors', 22.00, 'Varied', 'Solid', '3-Shirts', 'S', NULL),
 
-(5, 'Classic Shirt - Colored', 'Classic shirt made from high-quality cotton. Availible in 26 different colors', 22.00, 'Varied', 'Solid', '3-Shirts', 'M', NULL),
+('Classic Shirt - Colored', 'Classic shirt made from high-quality cotton. Available in 26 different colors', 22.00, 'Varied', 'Solid', '3-Shirts', 'M', NULL),
 
-(6, 'Classic Shirt - Colored', 'Classic shirt made from high-quality cotton. Availible in 26 different colors', 22.00, 'Varied', 'Solid', '3-Shirts', 'L', NULL),
+('Classic Shirt - Colored', 'Classic shirt made from high-quality cotton. Available in 26 different colors', 22.00, 'Varied', 'Solid', '3-Shirts', 'L', NULL),
 
-(7, 'Pattern Shirt - Basic', 'Classic shirt made from high-quality cotton. Availible in 12 different patterns', 25.00, 'White/Black', 'Varied', '3-Shirts', 'S', NULL),
+('Pattern Shirt - Basic', 'Classic shirt made from high-quality cotton. Available in 12 different patterns', 25.00, 'White/Black', 'Varied', '3-Shirts', 'S', NULL),
 
-(8, 'Pattern Shirt - Basic', 'Classic shirt made from high-quality cotton. Availible in 12 different patterns', 25.00, 'White/Black', 'Varied', '3-Shirts', 'M', NULL),
+('Pattern Shirt - Basic', 'Classic shirt made from high-quality cotton. Available in 12 different patterns', 25.00, 'White/Black', 'Varied', '3-Shirts', 'M', NULL),
 
-(9, 'Pattern Shirt - Basic', 'Classic shirt made from high-quality cotton. Availible in 12 different patterns', 25.00, 'White/Black', 'Varied', '3-Shirts', 'L', NULL),
+('Pattern Shirt - Basic', 'Classic shirt made from high-quality cotton. Available in 12 different patterns', 25.00, 'White/Black', 'Varied', '3-Shirts', 'L', NULL),
 
-(10, 'Character Shirt', 'Plain shirt with art of a character from select media printed on it', 30.00, 'White/Black', 'Graphic', 'GAP', 'S', NULL),
+('Character Shirt', 'Plain shirt with art of a character from select media printed on it', 30.00, 'White/Black', 'Graphic', 'GAP', 'S', NULL),
 
-(11, 'Character Shirt', 'Plain shirt with art of a character from select media printed on it', 30.00, 'White/Black', 'Graphic', 'GAP', 'M', NULL),
+('Character Shirt', 'Plain shirt with art of a character from select media printed on it', 30.00, 'White/Black', 'Graphic', 'GAP', 'M', NULL),
 
-(12, 'Character Shirt', 'Plain shirt with art of a character from select media printed on it', 30.00, 'White/Black', 'Graphic', 'GAP', 'L', NULL),
+('Character Shirt', 'Plain shirt with art of a character from select media printed on it', 30.00, 'White/Black', 'Graphic', 'GAP', 'L', NULL),
 
-(13, 'Pattern Shirt - Complex', 'Shirt with complex pattern, sourced from a third party source', 29.00, 'White/Black/Red', 'Varied', 'Nordstrom', 'S', NULL),
+('Pattern Shirt - Complex', 'Shirt with complex pattern, sourced from a third party source', 29.00, 'White/Black/Red', 'Varied', 'Nordstrom', 'S', NULL),
 
-(14, 'Pattern Shirt - Complex', 'Shirt with complex pattern, sourced from a third party source', 29.00, 'White/Black/Red', 'Varied', 'Nordstrom', 'M', NULL),
+('Pattern Shirt - Complex', 'Shirt with complex pattern, sourced from a third party source', 29.00, 'White/Black/Red', 'Varied', 'Nordstrom', 'M', NULL),
 
-(15, 'Pattern Shirt - Complex', 'Shirt with complex pattern, sourced from a third party source', 29.00, 'White/Black/Red', 'Varied', 'Nordstrom', 'L', NULL),
+('Pattern Shirt - Complex', 'Shirt with complex pattern, sourced from a third party source', 29.00, 'White/Black/Red', 'Varied', 'Nordstrom', 'L', NULL),
 
-(16, 'Luxury Shirt', 'Finely crafted shirt made from the highest quality materials. Sourced directly from renowned brand.', 65.00, 'White/Black/Blue', 'Logo', 'Lous Vetton', 'S', NULL),
+('Luxury Shirt', 'Finely crafted shirt made from the highest quality materials. Sourced directly from renowned brand.', 65.00, 'White/Black/Blue', 'Logo', 'Louis Vuitton', 'S', NULL),
 
-(17, 'Luxury Shirt', 'Finely crafted shirt made from the highest quality materials. Sourced directly from renowned brand.', 65.00, 'White/Black/Blue', 'Logo', 'Lous Vetton', 'M', NULL),
+('Luxury Shirt', 'Finely crafted shirt made from the highest quality materials. Sourced directly from renowned brand.', 65.00, 'White/Black/Blue', 'Logo', 'Louis Vuitton', 'M', NULL),
 
-(18, 'Luxury Shirt', 'Finely crafted shirt made from the highest quality materials. Sourced directly from renowned brand.', 65.00, 'White/Black/Blue', 'Logo', 'Lous Vetton', 'L', NULL),
+('Luxury Shirt', 'Finely crafted shirt made from the highest quality materials. Sourced directly from renowned brand.', 65.00, 'White/Black/Blue', 'Logo', 'Louis Vuitton', 'L', NULL),
 
-(19, 'Large Pattern Shirt', 'Shirt with large pattern that stretches across the entire front and to the back', 33.00, 'Blue/Red', 'Pattern', 'Vogue', 'M', NULL),
+('Large Pattern Shirt', 'Shirt with large pattern that stretches across the entire front and to the back', 33.00, 'Blue/Red', 'Pattern', 'Vogue', 'M', NULL),
 
-(20, 'Large Pattern Shirt', 'Shirt with large pattern that stretches across the entire front and to the back', 33.00, 'Blue/Red', 'Pattern', 'Vogue', 'L', NULL),
+('Large Pattern Shirt', 'Shirt with large pattern that stretches across the entire front and to the back', 33.00, 'Blue/Red', 'Pattern', 'Vogue', 'L', NULL)
