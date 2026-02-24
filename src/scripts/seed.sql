a879016623149e0e0fbdfd51da32ee827c2878ef
allenres <allen.resulidze@gmail.com>
2026-02-24 11:17:04 -0800

    prepare seed for database (colors)

--- src/scripts/seed.sql
+++ src/scripts/seed.sql
@@ -14,30 +14,30 @@ VALUES
 
 ('Classic Shirt - Colored', 'Classic shirt made from high-quality cotton. Available in 26 different colors', 22.00, 'Varied', 'Solid', '3-Shirts', 'L', NULL),
 
-('Pattern Shirt - Basic', 'Classic shirt made from high-quality cotton. Available in 12 different patterns', 25.00, 'White/Black', 'Varied', '3-Shirts', 'S', NULL),
+('Pattern Shirt - Basic', 'Classic shirt made from high-quality cotton. Available in 12 different patterns', 25.00, 'White', 'Varied', '3-Shirts', 'S', NULL),
 
-('Pattern Shirt - Basic', 'Classic shirt made from high-quality cotton. Available in 12 different patterns', 25.00, 'White/Black', 'Varied', '3-Shirts', 'M', NULL),
+('Pattern Shirt - Basic', 'Classic shirt made from high-quality cotton. Available in 12 different patterns', 25.00, 'Black', 'Varied', '3-Shirts', 'M', NULL),
 
-('Pattern Shirt - Basic', 'Classic shirt made from high-quality cotton. Available in 12 different patterns', 25.00, 'White/Black', 'Varied', '3-Shirts', 'L', NULL),
+('Pattern Shirt - Basic', 'Classic shirt made from high-quality cotton. Available in 12 different patterns', 25.00, 'White', 'Varied', '3-Shirts', 'L', NULL),
 
-('Character Shirt', 'Plain shirt with art of a character from select media printed on it', 30.00, 'White/Black', 'Graphic', 'GAP', 'S', NULL),
+('Character Shirt', 'Plain shirt with art of a character from select media printed on it', 30.00, 'White', 'Graphic', 'GAP', 'S', NULL),
 
-('Character Shirt', 'Plain shirt with art of a character from select media printed on it', 30.00, 'White/Black', 'Graphic', 'GAP', 'M', NULL),
+('Character Shirt', 'Plain shirt with art of a character from select media printed on it', 30.00, 'White', 'Graphic', 'GAP', 'M', NULL),
 
-('Character Shirt', 'Plain shirt with art of a character from select media printed on it', 30.00, 'White/Black', 'Graphic', 'GAP', 'L', NULL),
+('Character Shirt', 'Plain shirt with art of a character from select media printed on it', 30.00, 'White', 'Graphic', 'GAP', 'L', NULL),
 
-('Pattern Shirt - Complex', 'Shirt with complex pattern, sourced from a third party source', 29.00, 'White/Black/Red', 'Varied', 'Nordstrom', 'S', NULL),
+('Pattern Shirt - Complex', 'Shirt with complex pattern, sourced from a third party source', 29.00, 'White', 'Varied', 'Nordstrom', 'S', NULL),
 
-('Pattern Shirt - Complex', 'Shirt with complex pattern, sourced from a third party source', 29.00, 'White/Black/Red', 'Varied', 'Nordstrom', 'M', NULL),
+('Pattern Shirt - Complex', 'Shirt with complex pattern, sourced from a third party source', 29.00, 'Black', 'Varied', 'Nordstrom', 'M', NULL),
 
-('Pattern Shirt - Complex', 'Shirt with complex pattern, sourced from a third party source', 29.00, 'White/Black/Red', 'Varied', 'Nordstrom', 'L', NULL),
+('Pattern Shirt - Complex', 'Shirt with complex pattern, sourced from a third party source', 29.00, 'Red', 'Varied', 'Nordstrom', 'L', NULL),
 
-('Luxury Shirt', 'Finely crafted shirt made from the highest quality materials. Sourced directly from renowned brand.', 65.00, 'White/Black/Blue', 'Logo', 'Louis Vuitton', 'S', NULL),
+('Luxury Shirt', 'Finely crafted shirt made from the highest quality materials. Sourced directly from renowned brand.', 65.00, 'Blue', 'Logo', 'Louis Vuitton', 'S', NULL),
 
-('Luxury Shirt', 'Finely crafted shirt made from the highest quality materials. Sourced directly from renowned brand.', 65.00, 'White/Black/Blue', 'Logo', 'Louis Vuitton', 'M', NULL),
+('Luxury Shirt', 'Finely crafted shirt made from the highest quality materials. Sourced directly from renowned brand.', 65.00, 'Black', 'Logo', 'Louis Vuitton', 'M', NULL),
 
-('Luxury Shirt', 'Finely crafted shirt made from the highest quality materials. Sourced directly from renowned brand.', 65.00, 'White/Black/Blue', 'Logo', 'Louis Vuitton', 'L', NULL),
+('Luxury Shirt', 'Finely crafted shirt made from the highest quality materials. Sourced directly from renowned brand.', 65.00, 'White', 'Logo', 'Louis Vuitton', 'L', NULL),
 
-('Large Pattern Shirt', 'Shirt with large pattern that stretches across the entire front and to the back', 33.00, 'Blue/Red', 'Pattern', 'Vogue', 'M', NULL),
+('Large Pattern Shirt', 'Shirt with large pattern that stretches across the entire front and to the back', 33.00, 'Blue', 'Pattern', 'Vogue', 'M', NULL),
 
-('Large Pattern Shirt', 'Shirt with large pattern that stretches across the entire front and to the back', 33.00, 'Blue/Red', 'Pattern', 'Vogue', 'L', NULL)
+('Large Pattern Shirt', 'Shirt with large pattern that stretches across the entire front and to the back', 33.00, 'Red', 'Pattern', 'Vogue', 'L', NULL)
