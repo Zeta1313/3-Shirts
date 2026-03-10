1a024fad35319023c94f7f5311950ebe35ce4ea8
allenres <allen.resulidze@gmail.com>
2026-03-10 12:23:16 -0700

    bcrypt and hashing

---
+++ src/services/user.service.js
@@ -0,0 +1,39 @@
+import db from "../db/db.js";
+import bcrypt from "bcrypt";
+
+export const findUserByUsername = async (username) => {
+    const [results] = await db.query(
+        "SELECT userId, username, password, role FROM users WHERE username = ? LIMIT 1",
+        [username]
+    );
+    return results[0];
+};
+
+export const createUser = async (username, plainPassword, role = "user") => {
+    if (!username) throw new Error("Username is required.");
+    if (!plainPassword) throw new Error("Password is required.");
+    if(role !== "user" && role !== "admin") throw new Error("Invalid role.");
+
+    //hash the password before instert
+    const passwordHash = await hashPassword(plainPassword);
+
+    const [result] = await db.execute(
+        "INSERT INTO users (username, password, role) VALUES (?, ?, ?)",
+        [username, passwordHash, role]
+    );
+
+    return {
+        userId: result.insertId,
+        username,
+        role
+    };
+};
+
+export const hashPassword = async (plainPassword) => {
+    const saltRounds = 10;
+    return await bcrypt.hash(plainPassword, saltRounds);
+}
+
+export const validatePassword = async (plainPassword, storedHash) => {
+    return await bcrypt.compare(plainPassword, storedHash);
+};
