f008f055b5b34a8e8493aea2ae245a7827efb4b1
allenres <allen.resulidze@gmail.com>
2026-03-16 15:16:07 -0700

    fix routes

--- src/routers/cart.routes.js
+++ src/routers/cart.routes.js
@@ -5,9 +5,13 @@ import { requireAuth } from "../controllers/auth.controller.js";
 const router = Router();
 
 // protect api cart routes using isLoggedIn middleware
-router.get("/api/cart", requireAuth, cartController.allItemsInCart);
-router.post("/api/cart/items", requireAuth, cartController.addItemToCart);
-router.delete("/api/cart/items/:productId", requireAuth, cartController.deleteItemFromCart);
-router.post("/api/cart/clear", requireAuth, cartController.clearCart);
+// router.get("/api/cart", requireAuth, cartController.allItemsInCart);
+// router.post("/api/cart/items", requireAuth, cartController.addItemToCart);
+// router.delete("/api/cart/items/:productId", requireAuth, cartController.deleteItemFromCart);
+// router.post("/api/cart/clear", requireAuth, cartController.clearCart);
+router.get("/", cartController.allItemsInCart);
+router.post("/items", cartController.addItemToCart);
+router.delete("/items/:productId", cartController.deleteItemFromCart);
+router.post("/clear", cartController.clearCart);
 
 export default router;
